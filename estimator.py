acecab95f2c35ff6a3f14c9eafb2c960622c2bf7
Ugnius <ugnius.malukas@gmail.com>
2020-03-25 21:01:59 +0100

    Updated estimator.py.

--- estimator.py
+++ estimator.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import tensorflow as tf
 
@@ -15,30 +16,37 @@ def eval_input_fn(features, labels, batch_size):
     return dataset.batch(batch_size)
 
 
-# Read data from csv file and remove the first row.
-# Three  output classes setosa=0, versicolor=1, virginica=2.
-train_data = pd.read_csv("dataset/iris_training.csv", skiprows=1, names=['f1', 'f2', 'f3', 'f4', 'f5'])
-test_data = pd.read_csv("dataset/iris_test.csv", skiprows=1, names=['f1', 'f2', 'f3', 'f4', 'f5'])
+def run(train_set_path, test_set_path):
+    # Read data from csv file and remove the first row.
+    # Three  output classes setosa=0, versicolor=1, virginica=2.
+    train_data = pd.read_csv(train_set_path, skiprows=1, names=['f1', 'f2', 'f3', 'f4', 'f5'])
+    test_data = pd.read_csv(test_set_path, skiprows=1, names=['f1', 'f2', 'f3', 'f4', 'f5'])
 
-# Separate train data.
-train_x = train_data[['f1', 'f2', 'f3', 'f4']]
-train_y = train_data.loc[:, ['f5']]
+    # Separate train data.
+    train_x = train_data[['f1', 'f2', 'f3', 'f4']]
+    train_y = train_data.loc[:, ['f5']]
 
-# Separate test data.
-test_x = test_data[['f1', 'f2', 'f3', 'f4']]
-test_y = test_data.loc[:, ['f5']]
+    # Separate test data.
+    test_x = test_data[['f1', 'f2', 'f3', 'f4']]
+    test_y = test_data.loc[:, ['f5']]
 
-# Define feature columns
-feature_columns = [tf.feature_column.numeric_column(key=key) for key in train_x.keys()]
+    # Define feature columns
+    feature_columns = [tf.feature_column.numeric_column(key=key) for key in train_x.keys()]
 
-# Define classifier
-classifier = tf.estimator.LinearClassifier(feature_columns=feature_columns, n_classes=3)
+    # Define classifier
+    classifier = tf.estimator.LinearClassifier(feature_columns=feature_columns, n_classes=3)
 
-classifier.train(
-    input_fn=lambda: train_input_fn(train_x, train_y, 100),
-    steps=2000)
+    # Run training.
+    classifier.train(
+        input_fn=lambda: train_input_fn(train_x, train_y, 100),
+        steps=2000)
 
-eval_result = classifier.evaluate(
-    input_fn=lambda: eval_input_fn(test_x, test_y, 100))
+    # Run testing.
+    eval_result = classifier.evaluate(
+        input_fn=lambda: eval_input_fn(test_x, test_y, 100))
 
-print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+
+if __name__ == "__main__":
+    run(sys.argv[1], sys.argv[2])
